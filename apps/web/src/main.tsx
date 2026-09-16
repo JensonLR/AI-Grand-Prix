b3122a925aa84367e9b0b70589bafce1ca76af71
JensonLR <jensonlewis0@gmail.com>
2026-09-16 22:53:24 +0100

    Load responsive race weekend styling

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { ChampionshipApp } from './ChampionshipApp';
 import './styles.css';
 import './championship.css';
+import './weekend.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><ChampionshipApp/></StrictMode>);
