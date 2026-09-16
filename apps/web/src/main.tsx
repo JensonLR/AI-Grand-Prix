c83f6c040453254dd857e606f17e58835f58642a
JensonLR <jensonlewis0@gmail.com>
2026-09-16 22:45:06 +0100

    Switch browser app to responsive connectome championship

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,2 +1,7 @@
-import { StrictMode } from 'react';import { createRoot } from 'react-dom/client';import { App } from './App';import './styles.css';import './cwc.css';
-createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChampionshipApp } from './ChampionshipApp';
+import './styles.css';
+import './championship.css';
+
+createRoot(document.getElementById('root')!).render(<StrictMode><ChampionshipApp/></StrictMode>);
