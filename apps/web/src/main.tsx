9b24722e9118df34e1c5f54e7fa362d12bc14ab0
JensonLR <jensonlewis0@gmail.com>
2026-09-17 15:33:29 +0100

    Wire final presentation styles and deferred runtime cache

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChampionshipApp } from './ChampionshipApp';
-import { preloadBancRuntime } from './BancFullRuntime';
 import './styles.css';
 import './championship.css';
 import './weekend.css';
@@ -12,6 +11,11 @@ import './constructor-marks.css';
 import './development.css';
 import './product-v2.css';
 import './race-viewport.css';
+import './podium.css';
+import './final-polish.css';
 
-preloadBancRuntime();
 createRoot(document.getElementById('root')!).render(<StrictMode><ChampionshipApp/></StrictMode>);
+
+if(import.meta.env.PROD&&'serviceWorker' in navigator){
+  addEventListener('load',()=>{void navigator.serviceWorker.register(new URL('sw.js',document.baseURI)).catch(()=>undefined)},{once:true});
+}
